db320bde1a86a4f37dd95c3d22389366349e2b84
Munish Sharma <mnsh.sharma@outlook.com>
2026-01-09 19:00:02 +0530

    add aks

--- main.tf
+++ main.tf
@@ -6,3 +6,49 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+# Create a Virtual Network for AKS
+resource "azurerm_virtual_network" "aks_vnet" {
+  name                = "vnet-${var.cluster_name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = [var.vnet_address_space]
+}
+
+# Create a Subnet for AKS
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "subnet-${var.cluster_name}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes     = [var.subnet_address_space]
+}
+
+# Create Azure Kubernetes Service Cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.dns_prefix
+
+  default_node_pool {
+    name           = "default"
+    node_count     = var.node_count
+    vm_size        = var.vm_size
+    vnet_subnet_id = azurerm_subnet.aks_subnet.id
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    service_cidr   = var.service_cidr
+    dns_service_ip = var.dns_service_ip
+  }
+
+  tags = {
+    Environment = "Development"
+    ManagedBy   = "Terraform"
+  }
+}
--- output.tf
+++ output.tf
@@ -7,3 +7,43 @@ output "resource_group_name" {
   value       = azurerm_resource_group.rg.name
   description = "The name of the created resource group"
 }
+
+output "kubernetes_cluster_id" {
+  value       = azurerm_kubernetes_cluster.aks.id
+  description = "The ID of the AKS cluster"
+}
+
+output "kubernetes_cluster_name" {
+  value       = azurerm_kubernetes_cluster.aks.name
+  description = "The name of the AKS cluster"
+}
+
+output "kube_config" {
+  value       = azurerm_kubernetes_cluster.aks.kube_config_raw
+  description = "Kubernetes cluster config"
+  sensitive   = true
+}
+
+output "client_certificate" {
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate
+  description = "Client certificate for authentication"
+  sensitive   = true
+}
+
+output "client_key" {
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].client_key
+  description = "Client key for authentication"
+  sensitive   = true
+}
+
+output "cluster_ca_certificate" {
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate
+  description = "Cluster CA certificate"
+  sensitive   = true
+}
+
+output "host" {
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  description = "Kubernetes host endpoint"
+  sensitive   = true
+}
